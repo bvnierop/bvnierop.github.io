89d84be44504a54d3ef1423f4c072d00c346b41d
Bart van Nierop <mail@bvnierop.nl>
2026-09-25 20:51:59 +0200

    Add development flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Provides a development shell for bvnierop.github.io";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              python3
+            ];
+          };
+        }
+      );
+}
